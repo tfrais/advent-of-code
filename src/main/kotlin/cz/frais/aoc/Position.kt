43be95da3ef2f6a8542d4e2ab6fda5920977a24c
Tomas Frais <frais@email.cz>
2024-12-03 09:58:46 +0100

    Add Position distanceTo(pos) fun

--- src/main/kotlin/cz/frais/aoc/Position.kt
+++ src/main/kotlin/cz/frais/aoc/Position.kt
@@ -1,3 +1,9 @@
 package cz.frais.aoc
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+
+    fun distanceTo(destinationPosition: Position): Int {
+        return (destinationPosition.x - this.x) + (destinationPosition.y - this.y)
+    }
+
+}
---
+++ src/test/kotlin/cz/frais/aoc/PositionTest.kt
@@ -0,0 +1,17 @@
+package cz.frais.aoc
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PositionTest {
+
+    @Test
+    fun testDistanceToPosition() {
+        assertThat(Position(0, 0).distanceTo(Position(0, 0))).isEqualTo(0)
+        assertThat(Position(0, 0).distanceTo(Position(1, 0))).isEqualTo(1)
+        assertThat(Position(0, 0).distanceTo(Position(0, 3))).isEqualTo(3)
+        assertThat(Position(0, 0).distanceTo(Position(3, 0))).isEqualTo(3)
+        assertThat(Position(1, 0).distanceTo(Position(5, 5))).isEqualTo(9)
+    }
+
+}
