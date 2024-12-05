755913f4a892e5925375fc74d29248fbaca9eebf
Tomas Frais <frais@email.cz>
2024-12-05 09:21:21 +0100

    Add 2024/05 part 2 solution

--- src/main/kotlin/cz/frais/aoc/year2024/day05/Year2024Day05.kt
+++ src/main/kotlin/cz/frais/aoc/year2024/day05/Year2024Day05.kt
@@ -4,12 +4,10 @@ import cz.frais.aoc.AdventOfCodeDaySolution
 
 object Year2024Day05 : AdventOfCodeDaySolution {
 
-    private fun computePageOrderMap(pages: List<Int>): Map<Int, Int> =
-        pages.withIndex()
+    fun isSortedCorrectly(pageOrderRules: List<Pair<Int, Int>>, pages: List<Int>): Boolean {
+        val pageOrderMap = pages.withIndex()
             .associate { (index, page) -> page to index }
 
-    fun isSortedCorrectly(pageOrderRules: List<Pair<Int, Int>>, page: List<Int>): Boolean {
-        val pageOrderMap = computePageOrderMap(page)
         pageOrderRules.forEach {
             if (pageOrderMap.contains(it.first) && pageOrderMap.contains(it.second)) {
                 if (pageOrderMap[it.first]!! > pageOrderMap[it.second]!!) {
@@ -18,6 +16,16 @@ object Year2024Day05 : AdventOfCodeDaySolution {
             }
         }
         return true
+    }
+
+    fun sort(pageOrderRules: List<Pair<Int, Int>>, pages: List<Int>): List<Int> {
+        return pages.sortedWith { o1, o2 ->
+            when {
+                pageOrderRules.contains(Pair(o1, o2)) -> -1
+                pageOrderRules.contains(Pair(o2, o1)) -> 1
+                else -> o1.compareTo(o2)
+            }
+        }
     }
 
     private fun middleElement(page: List<Int>) = page[page.size / 2]
@@ -30,7 +38,11 @@ object Year2024Day05 : AdventOfCodeDaySolution {
     }
 
     override fun computePart2(input: String): Long {
-        TODO("Not yet implemented")
+        val parsedData = Parser.parse(input)
+        return parsedData.pages
+            .filter { !isSortedCorrectly(parsedData.pageOrderRules, it) }
+            .map { sort(parsedData.pageOrderRules, it) }
+            .sumOf { middleElement(it) }.toLong()
     }
 
 }
--- src/test/kotlin/cz/frais/aoc/year2024/Year2024Day05Test.kt
+++ src/test/kotlin/cz/frais/aoc/year2024/Year2024Day05Test.kt
@@ -3,7 +3,9 @@ package cz.frais.aoc.year2024
 
 import cz.frais.aoc.year2024.day05.Parser
 import cz.frais.aoc.year2024.day05.Year2024Day05.computePart1
+import cz.frais.aoc.year2024.day05.Year2024Day05.computePart2
 import cz.frais.aoc.year2024.day05.Year2024Day05.isSortedCorrectly
+import cz.frais.aoc.year2024.day05.Year2024Day05.sort
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -21,6 +23,21 @@ class Year2024Day05Test {
     fun testComputePart1() {
         val input = object {}.javaClass.getResource("/2024/day05_test_input.txt")!!.readText()
         assertThat(computePart1(input)).isEqualTo(143)
+    }
+
+    @Test
+    fun testSort() {
+        val input = object {}.javaClass.getResource("/2024/day05_test_input.txt")!!.readText()
+        val pageOrderRules = Parser.parse(input).pageOrderRules
+        assertThat(sort(pageOrderRules, listOf(13, 29))).containsExactly(29, 13)
+        assertThat(sort(pageOrderRules, listOf(29, 13))).containsExactly(29, 13)
+        assertThat(sort(pageOrderRules, listOf(61, 13, 29))).containsExactly(61, 29, 13)
+    }
+
+    @Test
+    fun testComputePart2() {
+        val input = object {}.javaClass.getResource("/2024/day05_test_input.txt")!!.readText()
+        assertThat(computePart2(input)).isEqualTo(123)
     }
 
 }
