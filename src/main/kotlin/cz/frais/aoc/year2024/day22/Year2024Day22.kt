d1e64f0b314d2686e39e24c212389229ba6ab744
Tomas Frais <frais@email.cz>
2024-12-25 10:50:59 +0100

    Add 2024/22 part 2 solution

--- src/main/kotlin/cz/frais/aoc/year2024/day22/Year2024Day22.kt
+++ src/main/kotlin/cz/frais/aoc/year2024/day22/Year2024Day22.kt
@@ -1,9 +1,13 @@
 package cz.frais.aoc.year2024.day22
 
 import cz.frais.aoc.AdventOfCodeDaySolution
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.util.*
 
 @Suppress("MagicNumber")
 object Year2024Day22 : AdventOfCodeDaySolution {
+
+    private val logger = KotlinLogging.logger {}
 
     fun nextSecretNumber(secretNumber: Long, nTimes: Int): Long {
         var result = secretNumber
@@ -24,12 +28,56 @@ object Year2024Day22 : AdventOfCodeDaySolution {
         return result.mix(result * 2048).prune()
     }
 
+    data class Part2SequenceElement(val price: Int, val priceChange: Int)
+
+    fun generatePart2Sequence(initialSecretNumber: Long, length: Int): List<Part2SequenceElement> {
+        val resultList = LinkedList<Part2SequenceElement>()
+        var currentSecretNumber = initialSecretNumber
+        resultList.add(Part2SequenceElement((currentSecretNumber % 10).toInt(), Int.MIN_VALUE))
+        repeat(length) {
+            currentSecretNumber = nextSecretNumber(currentSecretNumber)
+            val currentPrice = (currentSecretNumber % 10).toInt()
+            resultList.add(
+                Part2SequenceElement(
+                    currentPrice,
+                    currentPrice - resultList.last().price
+                )
+            )
+        }
+        return resultList.toList()
+    }
+
+    fun findChangesPriceInPart2Sequence(changes: List<Int>, sequence: List<Part2SequenceElement>): Int? {
+        val windowedSequence = sequence.map { it.priceChange }.windowed(4)
+        val priceIndex = windowedSequence.indexOf(changes)
+        return if (priceIndex != -1) sequence[priceIndex + 3].price else null
+    }
+
+    fun generateCombinations(range: IntRange, size: Int): List<List<Int>> =
+        if (size == 0) listOf(emptyList())
+        else range.flatMap { element ->
+            generateCombinations(range, size - 1).map { combination ->
+                listOf(element) + combination
+            }
+        }
+
     override fun computePart1(input: String): Long =
         input.lines().map { it.toLong() }
             .sumOf { nextSecretNumber(it, 2000) }
 
     override fun computePart2(input: String): Long {
-        TODO("Not yet implemented")
+        val inputSequences = input.lines().map { it.toLong() }.map { initialSecretNumber ->
+            generatePart2Sequence(initialSecretNumber, 2000)
+        }
+        var mostBananas: Long = Long.MIN_VALUE
+        for (changes in generateCombinations(-9..9, 4)) {
+            if (changes.drop(2) == listOf(-9, -9)) logger.info { "$changes, $mostBananas" }
+            val bananasSum = inputSequences.sumOf { sequence ->
+                findChangesPriceInPart2Sequence(changes, sequence)?.toLong() ?: 0
+            }
+            if (bananasSum > mostBananas) mostBananas = bananasSum
+        }
+        return mostBananas
     }
 
 }
--- src/test/kotlin/cz/frais/aoc/year2024/day22/Year2024Day22Test.kt
+++ src/test/kotlin/cz/frais/aoc/year2024/day22/Year2024Day22Test.kt
@@ -1,5 +1,8 @@
 package cz.frais.aoc.year2024.day22
 
+import cz.frais.aoc.year2024.day22.Year2024Day22.computePart2
+import cz.frais.aoc.year2024.day22.Year2024Day22.findChangesPriceInPart2Sequence
+import cz.frais.aoc.year2024.day22.Year2024Day22.generatePart2Sequence
 import cz.frais.aoc.year2024.day22.Year2024Day22.nextSecretNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -18,6 +21,30 @@ class Year2024Day22Test {
         assertThat(nextSecretNumber(10, 2000)).isEqualTo(4700978)
         assertThat(nextSecretNumber(100, 2000)).isEqualTo(15273692)
         assertThat(nextSecretNumber(2024, 2000)).isEqualTo(8667524)
+    }
+
+    @Test
+    fun testGeneratePart2Sequence() {
+        assertThat(generatePart2Sequence(123, 5)).containsExactly(
+            Year2024Day22.Part2SequenceElement(3, Int.MIN_VALUE),
+            Year2024Day22.Part2SequenceElement(0, -3),
+            Year2024Day22.Part2SequenceElement(6, 6),
+            Year2024Day22.Part2SequenceElement(5, -1),
+            Year2024Day22.Part2SequenceElement(4, -1),
+            Year2024Day22.Part2SequenceElement(4, 0)
+        )
+    }
+
+    @Test
+    fun testFindChangesPriceInPart2Sequence() {
+        val sequence = generatePart2Sequence(123, 10)
+        assertThat(findChangesPriceInPart2Sequence(listOf(-1, -1, 0, 2), sequence)).isEqualTo(6)
+        assertThat(findChangesPriceInPart2Sequence(listOf(1, 2, 3, 4), sequence)).isNull()
+    }
+
+    @Test
+    fun testComputePart2() {
+        assertThat(computePart2("1\n2\n3\n2024")).isEqualTo(23)
     }
 
 }
