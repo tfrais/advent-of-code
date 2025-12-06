e92f89d047c56ef486ec0c4c34ee8a4ef538cfaf
Tomas Frais <tomas.frais@gmail.com>
2025-12-06 21:23:37 +0100

    Finish 2025/06 part 2 solution

--- src/main/kotlin/cz/frais/aoc/year2025/day06/MathHomework.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day06/MathHomework.kt
@@ -1,5 +1,7 @@
 package cz.frais.aoc.year2025.day06
 
+import cz.frais.aoc.structures.Table
+
 data class MathHomework(
     val operands: List<List<Long>>,
     val operations: List<(Long, Long) -> Long>,
@@ -36,7 +38,43 @@ data class MathHomework(
         }
 
         fun fromStringPart2(input: String): MathHomework {
-            TODO("Not yet implemented")
+            val operations = input.lines().last().filter { !it.isWhitespace() }.reversed()
+                .map { symbol ->
+                    when (symbol) {
+                        '+' -> { l1: Long, l2: Long -> l1 + l2 }
+                        '*' -> { l1: Long, l2: Long -> l1 * l2 }
+                        else -> throw IllegalArgumentException("Unsupported operation symbol: $symbol")
+                    }
+                }.toList()
+
+            val initialAccumulatorValues = input.lines().last().filter { !it.isWhitespace() }.reversed()
+                .map { symbol ->
+                    when (symbol) {
+                        '+' -> 0L
+                        '*' -> 1L
+                        else -> throw IllegalArgumentException("Unsupported operation symbol: $symbol")
+                    }
+                }.toList()
+
+            val operandsTable = Table(input.lines().dropLast(1).joinToString(separator = "\n")) { it }
+            var operands = mutableListOf<List<Long>>()
+            var currentProblem = mutableListOf<Long>()
+            for (c in operandsTable.width() - 1 downTo 0) {
+                val columnString = operandsTable.getColumn(c)
+
+                // "Problems are still separated with a column consisting only of spaces"
+                if (columnString.all { it.isWhitespace() }) {
+                    operands.add(currentProblem)
+                    currentProblem = mutableListOf<Long>()
+                } else {
+                    currentProblem.add(
+                        columnString.filter { !it.isWhitespace() }.joinToString(separator = "").toLong()
+                    )
+                }
+            }
+            operands.add(currentProblem)
+
+            return MathHomework(operands, operations, initialAccumulatorValues)
         }
 
     }
--- src/main/kotlin/cz/frais/aoc/year2025/day06/Year2025Day06.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day06/Year2025Day06.kt
@@ -4,7 +4,8 @@ import cz.frais.aoc.AdventOfCodeDaySolution
 
 object Year2025Day06 : AdventOfCodeDaySolution {
 
-    fun compute(mathHomework: MathHomework): Long {
+    override fun computePart1(input: String): Long {
+        val mathHomework = MathHomework.fromStringPart1(input)
         var result = 0L
         for (i in mathHomework.operands[0].indices) {
             val operands = mathHomework.operands.map { it[i] }
@@ -13,10 +14,13 @@ object Year2025Day06 : AdventOfCodeDaySolution {
         return result
     }
 
-    override fun computePart1(input: String): Long =
-        compute(MathHomework.fromStringPart1(input))
-
-    override fun computePart2(input: String): Long =
-        compute(MathHomework.fromStringPart2(input))
+    override fun computePart2(input: String): Long {
+        val mathHomework = MathHomework.fromStringPart2(input)
+        var result = 0L
+        for (i in mathHomework.operands.indices) {
+            result += mathHomework.operands[i].fold(mathHomework.initialAccumulatorValues[i], mathHomework.operations[i])
+        }
+        return result
+    }
 
 }
