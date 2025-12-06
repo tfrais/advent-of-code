b34565904502f54399232c6973c3cf01723526eb
Tomas Frais <tomas.frais@gmail.com>
2025-12-06 23:46:52 +0100

    Polish 2025/03 solution

--- src/main/kotlin/cz/frais/aoc/year2025/day03/Year2025Day03.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day03/Year2025Day03.kt
@@ -4,44 +4,30 @@ import cz.frais.aoc.AdventOfCodeDaySolution
 
 object Year2025Day03 : AdventOfCodeDaySolution {
 
+    private const val PART1_DIGIT_COUNT = 2
     private const val PART2_DIGIT_COUNT = 12
 
-    fun maximumJoltagePart1(batteryBank: String): Int {
-        var max = 0;
-        for (firstDigitIndex in batteryBank.indices) {
-            for (secondDigitIndex in firstDigitIndex + 1 until batteryBank.length) {
-                val number = 10 * batteryBank[firstDigitIndex].digitToInt() +
-                        batteryBank[secondDigitIndex].digitToInt()
-                if (number > max) {
-                    max = number
-                }
-            }
-        }
-        return max;
-    }
+    fun greedyAlgorithm(batteryBank: String, digitCount: Int): Long {
+        val selectedDigits = mutableListOf<Int>()
+        var windowStart = 0
 
-    fun maximumJoltagePart2(batteryBank: String): Long {
-        val selectedDigits = mutableListOf<Pair<Int, Int>>()
-        var currentStartIndex = 0
-
-        while (selectedDigits.size < PART2_DIGIT_COUNT) {
-            val digitsToSelect = PART2_DIGIT_COUNT - selectedDigits.size
-            val windowEnd = batteryBank.length - digitsToSelect + 1
-            val searchPrefix = batteryBank.substring(currentStartIndex, windowEnd)
-            val maxDigitChar = searchPrefix.max()
-            val nextDigitIndexInPrefix = searchPrefix.indexOf(maxDigitChar)
-            val nextDigitOriginalIndex = currentStartIndex + nextDigitIndexInPrefix
-            selectedDigits.add(Pair(maxDigitChar.toString().toInt(), nextDigitOriginalIndex))
-            currentStartIndex = nextDigitOriginalIndex + 1
+        while (selectedDigits.size < digitCount) {
+            val remainingDigitCount = digitCount - selectedDigits.size
+            val windowEnd = batteryBank.length - remainingDigitCount + 1
+            val windowString = batteryBank.substring(windowStart, windowEnd)
+            val maxDigitChar = windowString.max()
+            val maxDigitCharIndexInWindow = windowString.indexOf(maxDigitChar) // note: taking the leftmost
+            selectedDigits.add(maxDigitChar.toString().toInt())
+            windowStart += maxDigitCharIndexInWindow + 1
         }
 
-        return selectedDigits.map { it.first }.joinToString(separator = "").toLong()
+        return selectedDigits.joinToString(separator = "").toLong()
     }
 
     override fun computePart1(input: String): Long =
-        input.lines().sumOf { maximumJoltagePart1(it) }.toLong()
+        input.lines().sumOf { greedyAlgorithm(it, PART1_DIGIT_COUNT) }
 
     override fun computePart2(input: String): Long =
-        input.lines().sumOf { maximumJoltagePart2(it) }
+        input.lines().sumOf { greedyAlgorithm(it, PART2_DIGIT_COUNT) }
 
 }
--- src/test/kotlin/cz/frais/aoc/year2025/day03/Year2025Day03Test.kt
+++ src/test/kotlin/cz/frais/aoc/year2025/day03/Year2025Day03Test.kt
@@ -2,27 +2,26 @@ package cz.frais.aoc.year2025.day03
 
 import cz.frais.aoc.year2025.day03.Year2025Day03.computePart1
 import cz.frais.aoc.year2025.day03.Year2025Day03.computePart2
-import cz.frais.aoc.year2025.day03.Year2025Day03.maximumJoltagePart1
-import cz.frais.aoc.year2025.day03.Year2025Day03.maximumJoltagePart2
+import cz.frais.aoc.year2025.day03.Year2025Day03.greedyAlgorithm
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Year2025Day03Test {
 
     @Test
-    fun testMaximumJoltagePart1() {
-        assertThat(maximumJoltagePart1("987654321111111")).isEqualTo(98)
-        assertThat(maximumJoltagePart1("811111111111119")).isEqualTo(89)
-        assertThat(maximumJoltagePart1("234234234234278")).isEqualTo(78)
-        assertThat(maximumJoltagePart1("818181911112111")).isEqualTo(92)
+    fun testGreedyAlgorithmPart1() {
+        assertThat(greedyAlgorithm("987654321111111", 2)).isEqualTo(98)
+        assertThat(greedyAlgorithm("811111111111119", 2)).isEqualTo(89)
+        assertThat(greedyAlgorithm("234234234234278", 2)).isEqualTo(78)
+        assertThat(greedyAlgorithm("818181911112111", 2)).isEqualTo(92)
     }
 
     @Test
-    fun testMaximumJoltagePart2() {
-        assertThat(maximumJoltagePart2("987654321111111")).isEqualTo(987654321111)
-        assertThat(maximumJoltagePart2("811111111111119")).isEqualTo(811111111119)
-        assertThat(maximumJoltagePart2("234234234234278")).isEqualTo(434234234278)
-        assertThat(maximumJoltagePart2("818181911112111")).isEqualTo(888911112111)
+    fun testGreedyAlgorithmPart2() {
+        assertThat(greedyAlgorithm("987654321111111", 12)).isEqualTo(987654321111)
+        assertThat(greedyAlgorithm("811111111111119", 12)).isEqualTo(811111111119)
+        assertThat(greedyAlgorithm("234234234234278", 12)).isEqualTo(434234234278)
+        assertThat(greedyAlgorithm("818181911112111", 12)).isEqualTo(888911112111)
     }
 
     @Test
