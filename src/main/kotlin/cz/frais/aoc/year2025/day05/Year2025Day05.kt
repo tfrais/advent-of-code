3d36ea77618fdb955d1b69a96c58f0d9287e54b0
Tomas Frais <frais@email.cz>
2025-12-06 13:07:38 +0100

    Add 2025/05 part 2 solution

--- src/main/kotlin/cz/frais/aoc/year2025/day05/Year2025Day05.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day05/Year2025Day05.kt
@@ -4,6 +4,34 @@ import cz.frais.aoc.AdventOfCodeDaySolution
 
 object Year2025Day05 : AdventOfCodeDaySolution {
 
+    fun haveOverlap(rangeA: LongRange, rangeB: LongRange): Boolean =
+        rangeA.first <= rangeB.last && rangeB.first <= rangeA.last
+
+    fun LongRange.merge(other: LongRange): LongRange =
+        minOf(this.first, other.first)..maxOf(this.last, other.last)
+
+    fun mergeRanges(ranges: List<LongRange>): List<LongRange> {
+        var mergedRanges = ranges.toMutableList()
+        do {
+            var changed = false
+            mergeLoop@ for (i in mergedRanges.indices) {
+                for (j in i + 1 until mergedRanges.size) {
+                    if (haveOverlap(mergedRanges[i], mergedRanges[j])) {
+                        changed = true
+
+                        val newRange = mergedRanges[i].merge(mergedRanges[j])
+                        mergedRanges.removeAt(j)
+                        mergedRanges.removeAt(i)
+                        mergedRanges.add(newRange)
+
+                        break@mergeLoop
+                    }
+                }
+            }
+        } while (changed)
+        return mergedRanges
+    }
+
     override fun computePart1(input: String): Long {
         val kitchenInventory = KitchenInventory.fromString(input)
         var result = 0L
@@ -16,7 +44,8 @@ object Year2025Day05 : AdventOfCodeDaySolution {
     }
 
     override fun computePart2(input: String): Long {
-        TODO("Not yet implemented")
+        return mergeRanges(KitchenInventory.fromString(input).freshIngredientRanges)
+            .sumOf { range -> range.last - range.first + 1 }
     }
 
 }
--- src/test/kotlin/cz/frais/aoc/year2025/day05/Year2025Day05Test.kt
+++ src/test/kotlin/cz/frais/aoc/year2025/day05/Year2025Day05Test.kt
@@ -1,6 +1,7 @@
 package cz.frais.aoc.year2025.day05
 
 import cz.frais.aoc.year2025.day05.Year2025Day05.computePart1
+import cz.frais.aoc.year2025.day05.Year2025Day05.computePart2
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -10,6 +11,12 @@ class Year2025Day05Test {
     fun testComputePart1() {
         val input = object {}.javaClass.getResource("/2025/day05_test_input.txt")!!.readText()
         assertThat(computePart1(input)).isEqualTo(3)
+    }
+
+    @Test
+    fun testComputePart2() {
+        val input = object {}.javaClass.getResource("/2025/day05_test_input.txt")!!.readText()
+        assertThat(computePart2(input)).isEqualTo(14)
     }
 
 }
