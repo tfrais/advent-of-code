7dee41c57e6852dd94f8a3e5937644e5545a65b8
Tomas Frais <tomas.frais@gmail.com>
2025-12-07 18:16:52 +0100

    Optimize 2025/07 part 2 solution

--- src/main/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07.kt
@@ -3,7 +3,6 @@ package cz.frais.aoc.year2025.day07
 import cz.frais.aoc.AdventOfCodeDaySolution
 import cz.frais.aoc.structures.Position
 import cz.frais.aoc.structures.Table
-import java.util.LinkedList
 
 object Year2025Day07 : AdventOfCodeDaySolution {
 
@@ -39,42 +38,21 @@ object Year2025Day07 : AdventOfCodeDaySolution {
 
     override fun computePart2(input: String): Long {
         val table: Table<DiagramElement> = Table(input) { DiagramElement.fromChar(it) }
+        val numberOfPaths = MutableList(table.width()) { 0L }
         val startPosition = table.findValue(DiagramElement.START).single()
+        numberOfPaths[startPosition.x] = 1
 
-        var finishedTimelinesCount = 0L
-        val timelinesInProgress = LinkedList<Timeline>()
-        timelinesInProgress.add(Timeline(mutableSetOf(startPosition.x), 1))
-
-        while (timelinesInProgress.isNotEmpty()) {
-            val currentTimeline = timelinesInProgress.removeLast()
-            if (currentTimeline.yPosition == table.height()) {
-                finishedTimelinesCount++;
-            } else {
-                var splitHappened = false
-                for (beamX in currentTimeline.beamsXPositions) {
-                    if (table.valueAt(Position(beamX, currentTimeline.yPosition)) == DiagramElement.SPLITTER) {
-                        splitHappened = true
-                        if (beamX > 0) {
-                            timelinesInProgress += Timeline(
-                                (currentTimeline.beamsXPositions - beamX + (beamX - 1)) as MutableSet<Int>,
-                                currentTimeline.yPosition
-                            )
-                        }
-                        if (beamX < table.width() - 1) {
-                            timelinesInProgress += Timeline(
-                                (currentTimeline.beamsXPositions - beamX + (beamX + 1)) as MutableSet<Int>,
-                                currentTimeline.yPosition
-                            )
-                        }
-                    }
-                }
-                if (!splitHappened) {
-                    timelinesInProgress += Timeline(currentTimeline.beamsXPositions, currentTimeline.yPosition + 1)
+        for (y in 1 until table.height()) {
+            for (x in 1 until table.width()) {
+                if (table.valueAt(Position(x, y)) == DiagramElement.SPLITTER) {
+                    numberOfPaths[x - 1] = numberOfPaths[x - 1] + numberOfPaths[x]
+                    numberOfPaths[x + 1] = numberOfPaths[x + 1] + numberOfPaths[x]
+                    numberOfPaths[x] = 0
                 }
             }
         }
 
-        return finishedTimelinesCount;
+        return numberOfPaths.sum()
     }
 
 }
