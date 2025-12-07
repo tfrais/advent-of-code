7f1c52cbe75c1892501ff66035c584e309ef38af
Tomas Frais <tomas.frais@gmail.com>
2025-12-07 12:51:28 +0100

    Add 2025/07 part 2 naive solution

--- src/main/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07.kt
+++ src/main/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07.kt
@@ -3,6 +3,7 @@ package cz.frais.aoc.year2025.day07
 import cz.frais.aoc.AdventOfCodeDaySolution
 import cz.frais.aoc.structures.Position
 import cz.frais.aoc.structures.Table
+import java.util.LinkedList
 
 object Year2025Day07 : AdventOfCodeDaySolution {
 
@@ -31,8 +32,49 @@ object Year2025Day07 : AdventOfCodeDaySolution {
         return splitCount
     }
 
+    private data class Timeline(
+        val beamsXPositions: MutableSet<Int>,
+        val yPosition: Int
+    )
+
     override fun computePart2(input: String): Long {
-        TODO("Not yet implemented")
+        val table: Table<DiagramElement> = Table(input) { DiagramElement.fromChar(it) }
+        val startPosition = table.findValue(DiagramElement.START).single()
+
+        var finishedTimelinesCount = 0L
+        val timelinesInProgress = LinkedList<Timeline>()
+        timelinesInProgress.add(Timeline(mutableSetOf(startPosition.x), 1))
+
+        while (timelinesInProgress.isNotEmpty()) {
+            val currentTimeline = timelinesInProgress.removeLast()
+            if (currentTimeline.yPosition == table.height()) {
+                finishedTimelinesCount++;
+            } else {
+                var splitHappened = false
+                for (beamX in currentTimeline.beamsXPositions) {
+                    if (table.valueAt(Position(beamX, currentTimeline.yPosition)) == DiagramElement.SPLITTER) {
+                        splitHappened = true
+                        if (beamX > 0) {
+                            timelinesInProgress += Timeline(
+                                (currentTimeline.beamsXPositions - beamX + (beamX - 1)) as MutableSet<Int>,
+                                currentTimeline.yPosition
+                            )
+                        }
+                        if (beamX < table.width() - 1) {
+                            timelinesInProgress += Timeline(
+                                (currentTimeline.beamsXPositions - beamX + (beamX + 1)) as MutableSet<Int>,
+                                currentTimeline.yPosition
+                            )
+                        }
+                    }
+                }
+                if (!splitHappened) {
+                    timelinesInProgress += Timeline(currentTimeline.beamsXPositions, currentTimeline.yPosition + 1)
+                }
+            }
+        }
+
+        return finishedTimelinesCount;
     }
 
 }
--- src/test/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07Test.kt
+++ src/test/kotlin/cz/frais/aoc/year2025/day07/Year2025Day07Test.kt
@@ -1,6 +1,7 @@
 package cz.frais.aoc.year2025.day07
 
 import cz.frais.aoc.year2025.day07.Year2025Day07.computePart1
+import cz.frais.aoc.year2025.day07.Year2025Day07.computePart2
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -10,6 +11,12 @@ class Year2025Day07Test {
     fun testComputePart1() {
         val input = object {}.javaClass.getResource("/2025/day07_test_input.txt")!!.readText()
         assertThat(computePart1(input)).isEqualTo(21)
+    }
+
+    @Test
+    fun testComputePart2() {
+        val input = object {}.javaClass.getResource("/2025/day07_test_input.txt")!!.readText()
+        assertThat(computePart2(input)).isEqualTo(40)
     }
 
 }
